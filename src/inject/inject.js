a00af2bb342a9b7b1b60a76958b6659e7cbc3cfa
Jay O'Connor <joconnor@lumoslabs.com>
2016-02-04 09:09:20 -0800

    working flowchart.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,14 +7,16 @@ console.log(list);
 for (var i = 0, len = list.length; i < len; i++) {
   if (list[i]['lang'] == 'flowchart') {
 		diagram_text = list[i].querySelector("code").innerHTML;
+		diagram_text = diagram_text.replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">");
 
 		// create a div on the page to display the diagram
 		newDiv = document.createElement("DIV");
-		insertAfter(newDiv, list[1]);
+		newDiv.setAttribute('id', 'flowchart_'+i);
+		insertAfter(newDiv, list[i]);
 
 		// draw the diagram
 		var diagram = flowchart.parse(diagram_text);
-		diagram.drawSVG(newDiv);
+		diagram.drawSVG('flowchart_'+i);
 	}
 
 	if (list[i]['lang'] == 'mermaid'){
@@ -24,7 +26,7 @@ for (var i = 0, len = list.length; i < len; i++) {
 		newDiv = document.createElement("DIV");
 		newDiv.setAttribute('class', 'mermaid');
 		newDiv.innerHTML = diagram_text;
-		insertAfter(newDiv, list[1]);
+		insertAfter(newDiv, list[i]);
 
 		mermaid.init({}, ".mermaid");
 	}
